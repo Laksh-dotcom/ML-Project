7212c66e8a3017c0337079b723076514d8d00691
Laksh-codes <lakshay.sharma@virtuos.com>
2025-12-29 18:41:57 +0530

    changes in logger and exception files

--- src/logger.py
+++ src/logger.py
@@ -1,19 +1,24 @@
 import logging
-import os 
+import os
 from datetime import datetime
 
-log_file = f"{datetime.now().strftime('%m_%d_%Y_%M_%S')}.log"
-log_path = os.path.join(os.getcwd(), "logs", log_file)
-os.makedirs(log_path, exist_ok = True)
+# Log file name
+log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-log_file_path = os.path.join(log_path, log_file)
+# Logs directory
+log_dir = os.path.join(os.getcwd(), "logs", log_file)
+
+# Create logs directory if not exists
+os.makedirs(log_dir, exist_ok=True)
+
+# Full log file path
+log_file_path = os.path.join(log_dir, log_file)
 
 logging.basicConfig(
-    filename = log_file_path,
-    format = "[%(asctime)s] %(lineno)d %(name)s %(levelname)s - %(message)s",
-    level = logging.INFO
+    filename=log_file_path,
+    format="[%(asctime)s] %(lineno)d %(name)s %(levelname)s - %(message)s",
+    level=logging.INFO,
 )
 
-if __name__ == "main":
+if __name__ == "__main__":
     logging.info("Logging has started.")
-
